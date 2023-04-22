6fcd6c253c4b61cdaf6c920282dc3ef7cd2cce0b
William Clifford <wobh@yahoo.com>
2023-04-21 23:10:50 -0400

    Setup testing with asdf, also fixed up some stuff in README
    - refactored tests into functions

--- org.wobh.common-lisp.games.starmaze.asd
+++ org.wobh.common-lisp.games.starmaze.asd
@@ -1,9 +1,17 @@
 ;; -*- mode: lisp -*-
 
-(asdf:defsystem :org.wobh.common-lisp.games.starmaze
-  :description ""
+(defsystem "org.wobh.common-lisp.games.starmaze"
+  :description "Common Lisp implementation of _Star Maze_"
   :version "0.0.1"
   :author "William Clifford <will@wobh.org>"
+  :in-order-to ((test-op (test-op "org.wobh.common-lisp.games.starmaze/test")))
   :components ((:file "starmaze")
-	       (:file "starmaze-test")
-	       (:file "starmaze-user")))
+               (:file "starmaze-user"
+                      :depends-on ("starmaze"))))
+
+(defsystem "org.wobh.common-lisp.games.starmaze/test"
+  :description "Tests for _Star Maze_"
+  :depends-on ("org.wobh.common-lisp.games.starmaze")
+  :perform (test-op (o c) (symbol-call 'starmaze-test
+                                       'test-all))
+  :components ((:file "starmaze-test")))
--- starmaze-test.lisp
+++ starmaze-test.lisp
@@ -1,20 +1,30 @@
 ;; -*- mode: lisp -*-
 
 (defpackage #:org.wobh.common-lisp.games.starmaze-test
-  (:use #:common-lisp)
+  (:nicknames #:starmaze-test)
   (:local-nicknames (#:starmaze #:org.wobh.common-lisp.games.starmaze))
+  (:use #:common-lisp)
+  (:export #:test-all)
   (:documentation "ORG.WOBH.COMMON-LISP.GAMES.STARMAZE-TEST
 "))
 
 (in-package #:org.wobh.common-lisp.games.starmaze-test)
 
-(assert (null (starmaze:list-games)))
+;; (assert (null (starmaze:list-games)))
 
-(let ((subject (starmaze:list-mazes)))
+(defun test-list-mazes
+    (&optional (subject (starmaze:list-mazes)))
   (setf (getf subject :foo)
 	'(1 2 3 4 5 6 7 8 9))
   (assert (null (getf (starmaze:list-mazes) :foo))
 	  ()
 	  "Mazes-db should be immutable through `starmaze:list-mazes'."))
+
+(defun test-all ()
+  (loop
+    with tests = '(test-list-mazes)
+    for test in tests
+    do (funcall test)
+    finally (return t)))
 
 ;; TODO: come up with better tests.
